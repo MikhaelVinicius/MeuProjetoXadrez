565dd7d4362b9d55cf5224f9c89ac3559af9e122
MikhaelVinicius <mickaelsoel@gmail.com>
2022-09-13 14:42:30 -0300

    criado a classe position

---
+++ src/position.java
@@ -0,0 +1,26 @@
+public class position {
+    private int row;
+    private int column;
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+
+
+
+}
