f97861835d2ffacc99bb4542fa4563094e7a5a81
MikhaelVinicius <mickaelsoel@gmail.com>
2022-09-14 16:48:02 -0300

    Criado as classes ChessMatch e ChessPiece mas ambas incompletas devido há outas classes

--- src/ChessMatch.java
+++ src/ChessMatch.java
@@ -1,2 +1,10 @@
 public class ChessMatch {
+    private int turn;
+    private Color currentPlayer;
+    private boolean check;
+    private boolean checkMate;
+    private ChessPiece enPassantVulnerable;
+    private ChessPiece promoted;
+
+
 }
---
+++ src/ChessPiece.java
@@ -0,0 +1,20 @@
+public class ChessPiece {
+    private Color color;
+    private int moveCount;
+
+    public ChessPosition getChessPosition(){
+
+    }
+
+    protected boolean isThereOpponentPiece(Position position){
+        return true;
+    }
+
+    protected void increaseMoceCount(){
+
+    }
+    protected void  descraeseMoveCount(){
+        
+    }
+
+}
